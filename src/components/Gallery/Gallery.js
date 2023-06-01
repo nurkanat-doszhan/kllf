7f6f27890c977ac0b99ff7d701ceb4af29b9d6f5
nurkanat-doszhan <nurkanat.doszhan@gmail.com>
2023-06-01 18:05:01 +0600

    Галерея ✔

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,8 +1,8 @@
 import './Gallery.scss'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import "swiper/css/pagination"
-import { Pagination } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation } from "swiper";
+import "swiper/css/navigation";
 
 const Gallery = () => {
   return (
@@ -13,11 +13,11 @@ const Gallery = () => {
           <div className="gallery">
             <Swiper
               slidesPerView={3}
-              spaceBetween={30}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
+              centeredSlides={true}
+              spaceBetween={10}
+              loop={true}
+              navigation={true}
+              modules={[Navigation]}
               className="swiperGallery"
             >
               <SwiperSlide>
@@ -36,6 +36,27 @@ const Gallery = () => {
                 <img src="gal_1.jpg" alt="" />
               </SwiperSlide>
             </Swiper>
+            {/*<Swiper
+              className="swiperGallery"
+              modules={[Pagination]}
+              spaceBetween={25}
+              slidesPerView={3}
+              loop={true}
+              pagination={{ clickable: true }}
+            >
+              {/* <SwiperSlide>
+                <img src="gal_1.jpg" alt="" />
+              </SwiperSlide> 
+              <SwiperSlide>Slide 1</SwiperSlide>
+              <SwiperSlide>Slide 2</SwiperSlide>
+              <SwiperSlide>Slide 3</SwiperSlide>
+              <SwiperSlide>Slide 4</SwiperSlide>
+              <SwiperSlide>Slide 5</SwiperSlide>
+              <SwiperSlide>Slide 6</SwiperSlide>
+              <SwiperSlide>Slide 7</SwiperSlide>
+              <SwiperSlide>Slide 8</SwiperSlide>
+              <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper> */}
           </div>
         </div>
       </div>
